70c44a0d9e8042a5e29ad1b8e5bb2572b163e3f5
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-17 22:36:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
